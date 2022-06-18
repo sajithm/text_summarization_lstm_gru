a7efbd74e35be868c29239f7909727acae1e097b
Sajith Madhavan <sajithm@msn.com>
2022-06-18 17:46:28 +0530

    Code Cleanup in Evaluation

--- train_test_eval.py
+++ train_test_eval.py
@@ -39,48 +39,9 @@ def train(params):
                 print("Starting the training ...")
                 train_model(model, b, params, ckpt, ckpt_manager, "{0}/{1}.txt".format(params["log_dir"], model_name))
                 model = None
- 
-'''
-def test(params):
-    assert params["mode"].lower() in ["test","eval"], "change training mode to 'test' or 'eval'"
-    assert params["beam_size"] == params["batch_size"], "Beam size must be equal to batch_size, change the params"
-
-    tf.compat.v1.logging.info("Building the model ...")
-    model = PGN(params)
-    
-    print("Creating the vocab ...")
-    vocab = Vocab(params["vocab_path"], params["vocab_size"])
-
-    print("Creating the batcher ...")
-    b = batcher(params["data_dir"], vocab, params)
-
-    print("Creating the checkpoint manager")
-    checkpoint_dir = "{}".format(params["checkpoint_dir"])
-    ckpt = tf.train.Checkpoint(step=tf.Variable(0), PGN=model)
-    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_dir, max_to_keep=params["max_checkpoints"])
-
-    path = params["model_path"] if params["model_path"] else ckpt_manager.latest_checkpoint
-    ckpt.restore(path)
-    print("Model restored")
-
-    for batch in b:
-        yield  beam_decode(model, batch, vocab, params)
 
 
-def test_and_save(params):
-    assert params["test_save_dir"], "provide a dir where to save the results"
-    gen = test(params)
-    with tqdm(total=params["num_to_test"],position=0, leave=True) as pbar:
-        for i in range(params["num_to_test"]):
-            trial = next(gen)
-            with open(params["test_save_dir"]+"/article_"+str(i)+".txt", "w") as f:
-                f.write("article:\n")
-                f.write(trial.text)
-                f.write("\n\nabstract:\n")
-                f.write(trial.abstract)
-            pbar.update(1)
-'''
-def evaluate_model(encoder, decoder, params):
+def predict(encoder, decoder, params):
     model_name = encoder + "_" + decoder
     
     print("Evaluating", model_name)
@@ -104,6 +65,7 @@ def evaluate_model(encoder, decoder, params):
     for batch in b:
         yield beam_decode(model, batch, vocab, params)
 
+
 def evaluate(params):
     assert params["mode"].lower() == "eval", "change training mode to 'eval'"
     assert params["beam_size"] == params["batch_size"], "Beam size must be equal to batch_size, change the params"
@@ -113,25 +75,15 @@ def evaluate(params):
         for d in cellTypes:
             model_name = e + "_" + d
             if not params["model_name"] or model_name.upper() == params["model_name"].upper():
-                gen = evaluate_model(e, d, params)
+                gen = predict(e, d, params)
                 reals = []
                 preds = []
                 with tqdm(total=params["max_num_to_eval"],position=0, leave=True) as pbar:
                     for i in range(params["max_num_to_eval"]):
-                        try:
-                            trial = next(gen)
-                            reals.append(trial.real_abstract)
-                            preds.append(trial.abstract)
-                            pbar.update(1)
-                            
-                            #print("\n\nActual Text")
-                            #print(trial.text)
-                            #print("\nReal Abstract")
-                            #print(trial.real_abstract)
-                            #print("\nPredicted Abstract")
-                            #print(trial.abstract)
-                        except Exception as ex: 
-                            print(ex)
+                        trial = next(gen)
+                        reals.append(trial.real_abstract)
+                        preds.append(trial.abstract)
+                        pbar.update(1)
                 r=Rouge()
                 scores = r.get_scores(preds, reals, avg=True)
                 print("\n\n")
